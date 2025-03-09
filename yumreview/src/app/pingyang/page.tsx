2e71a6eaf5c97d131473c941dc4d6d4edc0c69af
sopihtnapa22 <sopihtnapa@gmail.com>
2025-03-09 22:08:10 +0700

    ้ีhumyai

--- yumreview/src/app/pingyang/page.tsx
+++ yumreview/src/app/pingyang/page.tsx
@@ -1,10 +1,23 @@
+"use client";
+import RestaurantCard from '@/components/RestaurantCard';
 import React from 'react';
 
 const Pingyang: React.FC = () => {
     return (
-        <div>
-            <h1>ยินดีต้อนรับสู่หน้า ปิ้งย่าง</h1>
-            <p>ข้อมูลเกี่ยวกับเมนูปิ้งย่าง</p>
+        <div className="bg-yellow-100 min-h-screen flex flex-col items-center justify-start py-10">
+            {/* รูปภาพ */}
+            <img src="/ping.jpg" alt="PingYang" className="w-full h-[250px] object-cover rounded-lg  mt-0" />
+
+            <div style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                justifyContent: 'space-between',
+                padding: '20px'
+            }}>
+                <RestaurantCard name="ชื่อร้าน 1" id={'701b9ee9-9c56-4901-b662-db71c469a2d6'} imageUrl={'https://cms.dmpcdn.com/food/2023/02/23/45f22f80-b3ac-11ed-847b-5bfda9f44996_original.jpg'} />
+                <RestaurantCard name="ชื่อร้าน 2" id={'02702524-f208-43e3-9e02-94eafeb82a93'} imageUrl={'https://cms.dmpcdn.com/food/2023/02/23/45f22f80-b3ac-11ed-847b-5bfda9f44996_original.jpg'} />
+                <RestaurantCard name="ชื่อร้าน 3" id={''} imageUrl={'https://cms.dmpcdn.com/food/2023/02/23/45f22f80-b3ac-11ed-847b-5bfda9f44996_original.jpg'} />
+            </div>
         </div>
     );
 };
--- yumreview/src/components/Navbar.tsx
+++ yumreview/src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react';
 import { supabase } from '../../lib/supabase';
 import Link from 'next/link';
-import {   LogIn,  LogOut,  User } from 'react-feather';
+import { LogIn, LogOut, User } from 'react-feather';
 
 
 export default function Navbar() {
@@ -30,32 +30,35 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between items-center p-4 bg-white border-gray-200 ">
       <Link href="/">
-      <div className={"text-2xl font-bold text-black"}>YumReview</div>
+        <div className="text-4xl font-extrabold bg-gradient-to-r from-black to-gray-700 text-transparent bg-clip-text">
+          YumReview
+        </div>
+
       </Link>
 
       <div className="flex items-center space-x-4">
         {user ? (
-         <div className="relative">
-         <button
-           onClick={() => setShowLogout(!showLogout)}
-           className="p-2 rounded-lg hover:bg-gray-100 transition duration-200 cursor-pointer" 
-         >
-           <User size={24} className="text-purple-700" />
-         </button>
-       
-         {/* เมนู Logout */}
-         {showLogout && (
-           <div className="absolute top-full right-0 mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg z-[99]">
-             <button
-               onClick={handleLogout}
-               className="block w-full text-center px-4 py-2 text-gray-800 hover:bg-gray-100 transition-colors duration-200 cursor-pointer"
-             >
-               <LogOut size={24} className="text-purple-700" /> 
-              </button>
-           </div>
-         )}
-       </div>
-       
+          <div className="relative">
+            <button
+              onClick={() => setShowLogout(!showLogout)}
+              className="p-2 rounded-lg hover:bg-gray-100 transition duration-200 cursor-pointer"
+            >
+              <User size={24} className="text-purple-700" />
+            </button>
+
+            {/* เมนู Logout */}
+            {showLogout && (
+              <div className="absolute top-full right-0 mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg z-[99]">
+                <button
+                  onClick={handleLogout}
+                  className="block w-full text-center px-4 py-2 text-gray-800 hover:bg-gray-100 transition-colors duration-200 cursor-pointer"
+                >
+                  <LogOut size={24} className="text-purple-700" />
+                </button>
+              </div>
+            )}
+          </div>
+
         ) : (
           <Link href="/login">
             <div className="p-2 rounded-lg hover:bg-gray-100 transition duration-200">
